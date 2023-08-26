ceb524f5ed502aa9f3fd7702fdc84719cdb75622
JustJonathan37 <drawing4christ@gmail.com>
2023-08-26 10:57:28 -0700

    npm install and edit blueprint code

--- website/src/components/Home/index.tsx
+++ website/src/components/Home/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import { styles } from './styles';
+import styles from './styles';
 
 const Home = () => {
-  return <div style={styles.title}>Hello World!</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.heading}>Hello, World!</h1>
+      <p className={styles.paragraph}>This is a TypeScript React app.</p>
+    </div>
+  );
 };
 
 export default Home;
--- website/src/components/Home/styles.ts
+++ website/src/components/Home/styles.ts
@@ -1,8 +1,26 @@
-import styled from '@emotion/styled';
+import { css } from 'styled-components';
 
-export const styles = {
-  title: styled('div')`
-    size: 200px;
-    color: green;
-  `,
+const container = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
+const heading = css`
+  font-size: 3rem;
+  font-weight: bold;
+  margin-bottom: 1rem;
+`;
+
+const paragraph = css`
+  font-size: 1.5rem;
+  text-align: center;
+`;
+
+export default {
+  container,
+  heading,
+  paragraph,
 };
