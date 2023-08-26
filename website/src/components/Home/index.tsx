af972072819d2ea3e3268a8a8a148ead42de25a5
JustJonathan37 <drawing4christ@gmail.com>
2023-08-26 10:15:02 -0700

    Create a website home page

---
+++ website/src/components/Home/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { styles } from './styles';
+
+const Home = () => {
+  return <div style={styles.title}>Hello World!</div>;
+};
+
+export default Home;
---
+++ website/src/components/Home/styles.ts
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+export const styles = {
+  title: styled('div')`
+    size: 200px;
+    color: green;
+  `,
+};
